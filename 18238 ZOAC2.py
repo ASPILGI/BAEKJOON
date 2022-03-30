daacbc8a37337e0990eb4a80cbe5d51900ff22a9
ASPILGI <elysion715@gmail.com>
2022-03-30 22:38:33 +0900

    그리드 알고리즘
    그리드 알고리즘

---
+++ 18238 ZOAC2.py
@@ -0,0 +1,18 @@
+n = list(input())
+
+start = 'A'
+result = 0
+
+for i in n :
+  left_value = ord(start) - ord(i)
+  right_value = ord(i) - ord(start)
+
+  if left_value < 0 :
+    left_value += 26
+  elif right_value < 0 :
+    right_value += 26
+
+  result += min(left_value, right_value)
+  start = i
+
+print(result)
